20d07ccd8a0bb4fd07969907ac4492a4f1f2a9b1
VISVAA J <visvaajegadheesvaran@gmail.com>
2025-04-21 19:23:01 +0530

    Create 15. Alarm clock.java

---
+++ 15. Alarm clock.java
@@ -0,0 +1,67 @@
+package com.example.alarmclock;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    private TimePicker alarmTimePicker;
+    private PendingIntent pendingIntent;
+    private AlarmManager alarmManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        alarmTimePicker = findViewById(R.id.timePicker);
+        alarmTimePicker.setIs24HourView(true);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+    }
+
+    public void OnToggleClicked(android.view.View view) {
+        long time;
+        ToggleButton toggleButton = (ToggleButton) view;
+
+        if (toggleButton.isChecked()) {
+            Toast.makeText(this, "Alarm ON", Toast.LENGTH_SHORT).show();
+
+            Calendar calendar = Calendar.getInstance();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getHour());
+                calendar.set(Calendar.MINUTE, alarmTimePicker.getMinute());
+            } else {
+                calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+                calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+            }
+
+            calendar.set(Calendar.SECOND, 0);
+            calendar.set(Calendar.MILLISECOND, 0);
+
+            if (calendar.getTimeInMillis() <= System.currentTimeMillis()) {
+                calendar.add(Calendar.DAY_OF_MONTH, 1); // Schedule for next day
+            }
+
+            Intent intent = new Intent(this, AlarmReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(
+                    this, 0, intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+            );
+
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+        } else {
+            if (pendingIntent != null) {
+                alarmManager.cancel(pendingIntent);
+            }
+            Toast.makeText(this, "Alarm OFF", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
